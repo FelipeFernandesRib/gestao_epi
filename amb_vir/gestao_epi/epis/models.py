591ce5579fe1ea6d3e7f28863cd09a96a5527062
FelipeFernandesRib <lipe0602@gmail.com>
2025-09-25 21:16:14 -0300

    Otimiza o cálculo de estoque disponível no modelo Epi.

--- amb_vir/gestao_epi/epis/models.py
+++ amb_vir/gestao_epi/epis/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q, Sum
 
 class Epi(models.Model):
     nome = models.CharField(max_length=100)
@@ -10,25 +11,16 @@ class Epi(models.Model):
 
     @property
     def quantidade_disponivel(self):
-        emprestados = sum(
-            e.quantidade for e in self.emprestimo_set.filter(status="EMPRESTADO")
-        )
-        return self.quantidade_total - emprestados
-
-    def __str__(self):
-        return f"{self.nome}"
-    @property
-    def quantidade_disponivel(self):
         # Calcula o total emprestado que ainda não foi devolvido
-        emprestados = sum(
-            e.quantidade for e in self.emprestimo_set.filter(status="EMPRESTADO")
-        )
-        # EPIs perdidos já foram subtraídos da quantidade_total na view marcar_perdido
-        return self.quantidade_total - emprestados
-    
+        emprestados_count = self.emprestimo_set.filter(
+            Q(status="EMPRESTADO") | Q(status="EM_USO") | Q(status="FORNECIDO")
+        ).aggregate(total_emprestado=Sum('quantidade'))['total_emprestado'] or 0
+        return self.quantidade_total - emprestados_count
+
     @property
     def quantidade_perdida(self):
         # Para acompanhamento: quantos EPIs foram perdidos
-        return sum(
-            e.quantidade for e in self.emprestimo_set.filter(status="PERDIDO")
-        )
+        return self.emprestimo_set.filter(status="PERDIDO").aggregate(total_perdido=Sum('quantidade'))['total_perdido'] or 0
+
+    def __str__(self):
+        return f"{self.nome}"
